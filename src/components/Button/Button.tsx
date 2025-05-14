ca33d6e31b5e2860b41e2820d14e9b5c05e0ab63
Morphy <rrenat642@gmail.com>
2025-05-14 23:26:52 +0300

    2updt

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,9 @@
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
+  return <StyledButton {...props}>{children}</StyledButton>;
+};
+
+export default Button;
