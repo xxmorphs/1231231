61ca96954ee6efba792efbbb5320b7a4e2a8c73f
Morphy <rrenat642@gmail.com>
2025-05-17 00:19:02 +0300

    fixuseTelegram

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
-
+import svgr from 'vite-plugin-svgr'; // 1. Импортируйте плагин
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    svgr() // Добавляем плагин для SVG
+    svgr() // 2. Добавьте плагин в массив plugins
   ],
   server: {
     host: '0.0.0.0', // Для доступа с мобильного в локальной сети
     port: 3000      // Можете выбрать другой порт
   }
-});
+}); // ... ваши другие настройки Vite, если есть
